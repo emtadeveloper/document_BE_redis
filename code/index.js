f7610d65dac86a875b80eba9a1927c86e553e779
emtadeveloper <emta137671@gmail.com>
2024-12-01 14:06:24 +0330

    dsadasd

--- code/index.js
+++ code/index.js
@@ -1,20 +1,54 @@
 const express = require("express");
-
 const redis = require("redis");
-
-const client = redis.createClient();
-
-client.on("error", (err) => {
-  console.error("error", err);
-});
-
-client.on("connect", () => {
-  console.log("Connected To Redis");
-});
 
 const port = 3000;
 const app = express();
 
+const client = redis.createClient({
+  url: "redis://127.0.0.1:6379", // اطمینان از استفاده از IPv4
+});
+
+client.on("error", (err) => {
+  console.error("Error:", err);
+});
+
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to Redis");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to Redis:", err);
+  });
+
+app.get("/set", (req, res) => {
+  client.set("test", "value test");
+  res.send("your key is set !");
+});
+
+app.get("/get", async (req, res) => {
+  let test = await client.get("test");
+  res.send(`your key is ${test} !`);
+});
+
+app.get("/setjson", async (req, res) => {
+  const data = {
+    name: "moein",
+    age: 20,
+    mobile: "091541454",
+  };
+
+  await client.set("user:100", JSON.stringify(data));
+
+  res.send("your key is set !");
+});
+
+app.get("/getjson", async (req, res) => {
+  const stringData = await client.get("user:100");
+  const data = JSON.parse(stringData);
+  await client.set("user:100", JSON.stringify(data));
+  res.send(data);
+});
 app.listen(port, () => {
   console.log("App run ....");
 });
